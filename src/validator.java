840a0e8d7cad607ef710238d970c333467025f50
david bartz <the2baddavid@gmail.com>
2012-05-08 12:23:15 -0400

    Removed Unneeded Functions and Rebuilding validators

--- src/validator.java
+++ src/validator.java
@@ -24,27 +24,25 @@ public class validator {
     
     /**
      * Validate all the rows, using the validate two function for when there
-     * are more than two rows;  When only one, validate everything, Create 
-     * based on Height of structure If height is equal to 1, then return
-     * all rows as valid.
-     * 
-     * check rows specified in combo(row perm) or the row array
+     * are more than two rows
      * 
      * @param row permutation of rows to check
      */
-    static boolean validate_all(int combo_to_verify[]){
+    public boolean validate_all(int row_perm_to_check, int height){
         boolean verify = true;
         
-        for(int index = 1; index < height && verify == true; index++){
-            if (validate_two( row_array[combo_to_verify[index -1]], row_array[combo_to_verify[index]])){
-                verify = true;
+        // While Still Possible to be a valid row, continue
+        for(int index = 1; index < height; index++){
+            if ( validate_two() ){
+                // Continue
             }
+            // Otherwise Exit
             else{
-                verify = false;
+                return false;
             }
         }
-        
-        return verify;
+        // If it makes it through, then it is valid
+        return true;
     }
     
     /**
@@ -56,82 +54,11 @@ public class validator {
      * @param array2
      * @return whether the rows are valid together
      */
-    static boolean validate_two(double array1[], double array2[]){
+    static boolean validate_two(){
         boolean cont = true;
-        double sum1[] = new double[rowLength];
-        double sum2[] = new double[rowLength];
-        Hashtable valid = new Hashtable();
-        
-        // Start Summing
-        sum1[0] = array1[0];
-        sum2[0] = array2[0];
-        
-        // Loop through the rest
-        for(int index = 1; index < rowLength; index++){
-            sum1[index] = sum1[index-1] + array1[index];
-            sum2[index] = sum2[index-1] + array2[index];
-        }
-        
-        // Check for Collisions
-        for(int index = 0; index < rowLength && cont == true; index++){
-            valid.put(sum1[index], new Double(index));
-            if (valid.containsValue(sum2[index])){
-                cont = false;
-                break; // TODO Might cause issues?
-            }
-        }
+
+        // for loop to see if one contains an element of the other
         
         return cont;
-    }
-    
-    
-    /**
-     * Adds up all the elements in the array, then returns the answer;
-     * Used verify that the permutation of row is the correct length
-     * 
-     * @param test
-     * @return Returns the Sum
-     */
-    static double array_adder(double[] test){
-        double sum = 0;
-        
-        for(int index = 0; index < rowLength; index++){
-            sum += test[index];
-        }
-        
-        return sum;
-    }
-    
-    /**
-     * Recursive function is used to create permutations of possible combinations
-     * of single rows.
-     * 
-     * However, as there are only 2 possible values the behavior is not all that 
-     * different from binary counting permutation
-     * 
-     * @param old Array in progress, to be copied for more possibilities
-     * @param value value to be added to the array
-     * @param index current position in the array
-     */
-    static void builder(double old[], double value, int index){
-        // Unique Instance of the Array, and copy current to it
-        double test[] = new double[rowLength];
-        System.arraycopy(old,0,test,0,rowLength);
-        
-        if(index > 0){
-            test[index-1] = value;
-        }
-                
-        //  Test if we are at the end && that it is a valid answer
-        if(index == 0 && (array_adder(test) == length)){
-            // If valid, add the array to the list of possibles
-            System.arraycopy(test, 0, row_array[row_number], 0, rowLength);
-            row_number++;
-        }
-        //  Otherwise keep going!
-        else{
-            builder(test, brick1, (index-1));
-            builder(test, brick2, (index-1));
-        }
-    }
+    }    
 }
