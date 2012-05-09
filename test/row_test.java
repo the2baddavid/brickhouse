a20901e70137fb6179546b26fba9d847d3d26861
david bartz <the2baddavid@gmail.com>
2012-05-09 06:44:10 -0400

    Added JUnit Test for Row Object

---
+++ test/row_test.java
@@ -0,0 +1,80 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.ArrayList;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author david
+ */
+public class row_test {
+
+    double test_row[] = { 3, 3, 4.5, 3};
+    double row_length = 13.5;
+    int array_length = 4;
+    row test_object;    
+    
+    public row_test() {}
+    
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+        
+        
+        test_object = new row(test_row,row_length,array_length);
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    @Test
+    public void test_get_row(){
+        double expected[] = {};
+        double result [] = test_object.get_row();
+        
+        for (int index =0; index < array_length; index++)
+            assertEquals(expected[index], result[index], .00001);
+    }
+    
+    public void test_get_row_size(){
+        String input = null;
+        double expected = row_length;
+        double result = test_object.get_row_size();
+        
+        assertEquals(expected, result, .0001);        
+    }
+    
+    public void test_get_row_length(){
+        String input = null;
+        double expected = row_length;
+        double result = test_object.get_row_length();
+        
+        assertEquals(expected, result, .0001);
+    }
+    
+    public void test_get_hash(){
+        String input = null;
+        ArrayList expected[];
+        ArrayList result = test_object.get_hash();
+        
+        // TODO: Create Hash Test
+        //assertEquals(expected, result, .0001);
+    }
+}
