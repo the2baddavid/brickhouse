677900c67c756ed37f3485b72c9b706a481bb0a7
david bartz <the2baddavid@gmail.com>
2012-05-13 23:03:45 -0400

    commented time counter

--- src/time_counter.java
+++ src/time_counter.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Counter class is used for stop watching the runtime
  * functions exists for starting, stopping and returning runtime
@@ -13,14 +8,24 @@ class time_counter{
     double start;
     double finish;
     
+    /**
+     * Start time - Saves current time
+     */
     public void start(){
         start = System.currentTimeMillis();
     }
     
+    /**
+     * Stop time - Saves current time
+     */
     public void end(){
         finish = System.currentTimeMillis();
     }
     
+    /**
+     * Begets runtime by subtracting the start and end times
+     * @return 
+     */
     public double runTime(){
         return finish-start;
     }    
